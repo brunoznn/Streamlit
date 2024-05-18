b6e8150d68b19cb59ccb03486c80d242d749d233
Bruno Magalhães <167024132+brunoznn@users.noreply.github.com>
2024-05-18 20:11:51 -0300

    myapp.py

--- myapp.py
+++ myapp.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.title("Ainda não é domingo!")
+def baloes():
+    st.balloons()
+
+st.subheader("Nada não :)")
+v1 = st.button("Clique aqui (2x)")
+
+if 'show_text' not in st.session_state:
+    st.session_state.show_text = False
+
+def toggle_text():
+    st.session_state.show_text = not st.session_state.show_text
+
+if st.session_state.show_text:
+    st.subheader("18 anos já? velha demais em, credo. Já pode ser presa agora.")
+    st.subheader("PARABÉNS DODORA!! Muitos anos de vida e saúde, tmj.")
+
+if v1:
+    toggle_text()
+
+if v1:
+    baloes()
